b2fe062aaeea3ac77e107070b4bd58fc136202f6
stephenhage <stephenhage2017@u.northwestern.edu>
2020-04-03 16:07:22 -0500

    Using selenium to get other works that weren't included in nltk

--- shakespeare_works_parse.py
+++ shakespeare_works_parse.py
@@ -1,6 +1,13 @@
 from nltk.corpus import shakespeare
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
 import numpy as np
+
+DRIVER_PATH = '/Users/stephenhage/Downloads/chromedriver.exe'
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get('http://shakespeare.mit.edu/')
+
 
 nltk.download('shakespeare')
 shakespeare.xml('dream.xml')
